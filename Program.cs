37b960089124ee17a133889b2189c12f2e712b21
rfranett <rfranett@gmail.com>
2024-01-02 19:23:40 -0300

    novidades

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Array de Inteiros .\n");
+
+int[] arrayInteiros = new int[4];
+
+arrayInteiros[0] = 73;
+arrayInteiros[1] = 11;
+arrayInteiros[2] = 10;
+arrayInteiros[3] = 9;
+
+Console.WriteLine("Percorrendo o Array com o For");
+for (int contador = 0 ; contador < arrayInteiros.Length; contador++)
+{
+    Console.WriteLine($"posicão N° {contador} - {arrayInteiros[contador]}");
+}
+
+Console.WriteLine("Percorrendo o Array com o Foreach");
+
+int ContadorForeach = 0;
+foreach(int valor in arrayInteiros)
+{
+    Console.WriteLine($"Posição N° {ContadorForeach} - {valor}");
+    ContadorForeach ++;
+}
